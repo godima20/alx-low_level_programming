d0fb17271322442e204d7f92dd8eb760f00ee038
Gyvira Owiti <godima20@gmail.com>
2022-06-02 12:36:24 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description - prints various types of the computer
+ *
+ * Return: return always 0
+ */
+int main(void)
+{
+	printf("Size of char: %d bytes\n", sizeof(char));
+	printf("Size of an int: %d bytes\n", sizeof(int));
+	printf("Size of a long int: %d bytes\n", sizeof(int));
+	printf("Size of a long long int: %d bytes\n", sizeof(int));
+	printf("Size of a float: %d bytes\n", sizeof(int));
+	return (0);
