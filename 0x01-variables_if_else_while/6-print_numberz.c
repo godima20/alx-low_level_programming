ab1bb2c90d621ffd7ca64b3823c115ce0c29a33d
Gyvira Owiti <godima20@gmail.com>
2022-06-03 13:50:56 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description - Prints all single numbers of base 10 
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num % 10 + '0');
+	putchar('\n');
+	return (0);
+}
