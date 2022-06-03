c5c123cf4c27511bab3a96766597a31be8fc45e8
Gyvira Owiti <godima20@gmail.com>
2022-06-03 14:51:35 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description - Prints all possible combination of single digit numbers
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
