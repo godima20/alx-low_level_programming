67a774266a379ad9b36da6da0dbf6a2df7c886d9
Gyvira Owiti <godima20@gmail.com>
2022-06-03 10:31:55 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,8 +22,7 @@ int main(void)
 	}
 	else if ((n % 20) < 6 && (n % 20) != 0);
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 20);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 20);
 	}
 	else
 	{
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -entry point of the program
+ *
+ * Description - Prints alphabets in lowercase
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char alphabet ;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	putchar(alphabet);
+	
+	putchar('\n');
+	return (0);
+}
