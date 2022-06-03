295abd1d5d22300dbde75a8fe4b05eb73d7d3eed
Gyvira Owiti <godima20@gmail.com>
2022-06-03 11:05:30 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,7 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
-putchar(alphabet);
-putchar('\n');
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++); putchar(alphabet);
+	putchar('\n');
 	return (0);
 }
