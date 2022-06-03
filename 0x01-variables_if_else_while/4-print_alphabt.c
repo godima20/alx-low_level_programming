cd9d0e992de971426884e5477e6ee769bf843787
Gyvira Owiti <godima20@gmail.com>
2022-06-03 13:11:55 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description - Print all letters except q and e
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet = < 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
