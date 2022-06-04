ef7b71f56c2baa992f884c34cea099aa75eb21a2
Gyvira Owiti <godima20@gmail.com>
2022-06-04 12:26:35 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
-			for (num3 = num2 + 1; num3 < 10; num3++)
+			for (num3 = num2 = 1; num3 < 10; num3++)
 				putchar(num1 + '0');
 				putchar(num2 + '0');
 				putchar(num3 + '0');
