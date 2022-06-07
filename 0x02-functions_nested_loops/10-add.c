038032ee280da5ffe077295fb5e8732115f022c2
godima20 <godima20@gmail.com>
2022-06-07 21:58:02 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - Adds two integers and returns the result
+ * @num1: - The first integer to be added
+ * @num2: - The second integer to be added
+ *
+ * Return: Return the result
+ */
+int add(int, int)
+{
+	int num1, num2;
+	int result;
+
+	int result = int add(num1 + num2);
+	{
+		return(result);
+	}
+}
