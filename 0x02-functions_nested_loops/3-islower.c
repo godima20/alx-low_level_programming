4e3a4ba3491a4672992cf4015d5821afd779866a
godima20 <godima20@gmail.com>
2022-06-07 18:24:36 +0000

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,6 @@
 #include "main.h"
 /**
- * main - entry point of the program
- *
- * Description - Checks for lowercase character
- * @c: The chaaracter to be checked
+ *  _islower - checks for lowercase character
  *
  * Return: 1 if the character is lowercase and 0 if otherwise
  */
