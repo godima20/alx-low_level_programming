0dc1706ac76386a1f988ab29ae4fffacba9fc9bd
godima20 <godima20@gmail.com>
2022-06-07 19:45:54 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute integer value
+ * @b: - is the integer to be compiled
+ *
+ * Return: Always returns 0
+ */
+int _abs(int b)
+{
+	if (b >= 0)
+		return (n);
+	else
+		return(-n)
+}
