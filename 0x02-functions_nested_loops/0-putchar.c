c66427451ea489b6a28e603fc739ce992683f936
godima20 <godima20@gmail.com>
2022-06-07 10:32:34 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description - Prints _putchar
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{	char _putchar;
+
+	printf("_putchar" '\n');
+	return (0);
+}
