709b2d571d6e986697c2b80b9f45358510c67be2
godima20 <godima20@gmail.com>
2022-06-07 12:43:24 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ *
+ * Description - Prints the alphabets in lowercase
+ *
+ * Return: Always return 0
+ */
+void print_alphabet(void);
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+	return (0);
+}
