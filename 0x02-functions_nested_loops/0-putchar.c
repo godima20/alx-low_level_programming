4a355dee08ea417b0860f72e292200b8a81b48f7
godima20 <godima20@gmail.com>
2022-06-08 12:11:19 +0000

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,15 @@
  */
 int main(void)
 {
-	char n[] = "_putchar";
-	int b;
-
-	for (b = 0; b < 8; b++)
-	{
-	_putchar(n[b]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
