8d018f8dc0feb218db8ca5ebbd0066fd4418201b
godima20 <godima20@gmail.com>
2022-06-13 15:56:02 +0000

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@ int _putchar(char c);
  */
 void print_rev(char *s)
 {
-	int length = 0, index;
+	int i = 0;
 
-	while (s[index++])
-		length++;
-	for (index = length - 1; index >= 0; index--)
-		_putchar(s[index]);
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
