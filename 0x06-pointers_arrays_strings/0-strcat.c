c49e98c62c1e5d7236210d8a7bb818b03a8a0e80
godima20 <godima20@gmail.com>
2022-06-15 09:17:00 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcat - concanates the string pointed to by @src
+ * including the terminating null byte
+ * @dest: a pointer to the string to be concaneted
+ * @src: the source string to be appended to @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int s = 0, dest_len = 0;
+
+	while (dest[s++])
+		dest_len++;
+	for (s = 0; src[s]; s++)
+		dest[dest_len++] = src[s];
+	return (dest);
+}
