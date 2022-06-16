d437f8da9ca7b310546f3011e2f7cf8a5af254cf
godima20 <godima20@gmail.com>
2022-06-16 15:02:28 +0000

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,14 +4,18 @@
  *
  * @dest: a pointer to the string to be concaneted
  * @src: the source string to be appended to @dest
+ * Return: return @dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int s = 0, dest_len = -1;
+	int s = -1, dest_len;
 
-	while (dest[s++])
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	;
+	do {
+		s++;
+		dest[dest_len] = src[s];
 		dest_len++;
-	for (s = 0; src[s]; s++)
-		dest[dest_len++] = src[s];
+	} while (src[s] != '\0');
 	return (dest);
 }
