8943f06a1e721a65911bf06b077bede59beb5eb8
godima20 <godima20@gmail.com>
2022-06-22 11:42:51 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -26,9 +26,9 @@ int is_prime_number(int n)
 {
 	int div = 2;
 
-	if (n <= 2 && n <= 3)
-		return (1);
-	else if (n < 1)
+	if (n <= 1)
 		return (0);
+       	if (n >= 2 && n <= 3)
+		return (1);
 	return (is_divisible(n, div));
 }
