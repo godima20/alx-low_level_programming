b8ce61a0ec8482659410a6eb6e3fe95fa45b3ad7
godima20 <godima20@gmail.com>
2022-07-03 20:37:41 +0000

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Gyvira O Odima
+ * Desc: header files that defines a function
+ * like macro ABS(x) that computes absolute value of x
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif
