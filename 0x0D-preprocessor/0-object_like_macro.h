38c2d47d9bd6c58b368071f460af9f8886292b3f
godima20 <godima20@gmail.com>
2022-07-03 20:09:16 +0000

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/*
+ * File:0-object_like_macro.h
+ * Auth: Gyvira O Odima
+ * Desc: header file that defines a macro named
+ * SIZE as an abbreviation for 1024
+ */
+#define SIZE 1024
+#endif
