0249194e8fe94993ba03e267f524bb08b4e57073
godima20 <godima20@gmail.com>
2022-07-03 20:45:23 +0000

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+/*
+ * File: 4-sum.h
+ * Auth: Gyvira O Odima
+ * Desc: header file that defines a function like macro
+ * SUM(x, y) that computes the sum of numbers x and y
+ */
+#define SUM(x, y) ((x) + (y))
+#endif
