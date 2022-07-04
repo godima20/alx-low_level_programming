16e271ca0ab2c7a188026a9713da9289f1a06492
godima20 <godima20@gmail.com>
2022-07-04 16:13:33 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
  /**
-  * struct dog - a new type describing a dog
+  * struct dog - A new type describing a dog
   * @name: the name of the dog
   * @age: the age of the dog
   * @owner: the owner of the dog
