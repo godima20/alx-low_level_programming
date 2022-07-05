1b80b78ebf60d8e3f946000b7eba589e08e1b28b
godima20 <godima20@gmail.com>
2022-07-05 11:29:30 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
- /**
-  * struct dog - A new type describing a dog
-  * @name: the name of the dog
-  * @age: the age of the dog
-  * @owner: the owner of the dog
-  */
+/**
+ * struct dog - A dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
