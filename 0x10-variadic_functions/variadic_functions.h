ac60721b42d297407ec7bfb6a4d016a38e37f1da
godima20 <godima20@gmail.com>
2022-07-07 10:46:23 +0000

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,3 +3,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #include <stdarg.h>
+int _putchar(char c);
