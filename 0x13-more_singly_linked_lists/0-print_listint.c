950849a36e193314914e10832fd2046d74509629
godima20 <godima20@gmail.com>
2022-07-18 15:20:27 +0000

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints all elements of a list
+ * @h: list
+ *
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num_nodes += 1;
+		h = h->next;
+	}
+	return (num_nodes);
+}
