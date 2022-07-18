492f4d03aa12785a91f6f2d4d9e7f45261a4aa6e
godima20 <godima20@gmail.com>
2022-07-18 11:31:48 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <typedef.h>
+/**
+ * print_list - prints all elements in a list_t list
+ * @h: A pointer to the list to iterate
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	size_t index = 0;
+
+	if (h && h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		tmp = h;
+		if (tmp)
+			tmp = h;
+		while (tmp)
+		{
+			printf("[%d] %s\n", tmp->len, tmp->str);
+			index++;
+			tmp = tmp->next;
+		}
+	}
+	return (index);
+}
