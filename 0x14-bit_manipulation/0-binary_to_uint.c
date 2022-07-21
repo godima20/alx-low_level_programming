14acf575b58a7db98887f2b2a60ceb4fe2596229
godima20 <godima20@gmail.com>
2022-07-21 09:25:40 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,24 +7,31 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, i;
-	unsigned int sum = 0;
+	unsigned int len;
+	unsigned int i;
+	unsigned int j;
+	unsigned int sum;
+	unsigned int pow;
+	int base;
 
+	base = 2;
+	sum = 0;
+	pow = 1;
 	if (b == NULL)
-		return (sum);
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-	i = 0;
-	while (b[i])
+		return (0);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+		return ((b[0] - 48));
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		for (j = len - 1; j > 0; j--)
+			pow = pow * base;
+		sum = sum + (pow * (b[i] - 48));
 		len--;
+		pow = 1;
 	}
 	return (sum);
 }
-
