9c27fd5f95435c3c07608639c735801092d24e7d
godima20 <godima20@gmail.com>
2022-09-01 12:02:42 +0000

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	newNode->next = *head;
 
-	if (head)
+	if (*head)
 		(*head)->prev = newNode;
 	*head = newNode;
 	return (newNode);
