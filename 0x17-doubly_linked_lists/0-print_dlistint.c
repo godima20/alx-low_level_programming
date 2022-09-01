57908a3e74899368a578fab7ebdc1ed7aa5ea74f
godima20 <godima20@gmail.com>
2022-09-01 07:17:37 +0000

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	const dlistint_t *current = h;
+	size_t elements = 0;
 
-	while (h)
+	while (current)
 	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+
+		current = current->next;
+		elements++;
 	}
-	return (nodes);
+	return (elements);
 }
